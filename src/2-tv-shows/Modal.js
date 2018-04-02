3420704b360938eaf7dbfb4d027b5219659d1fa8
Nicolas Payot <nicolas.payot@gmail.com>
2018-04-02 17:59:41 +0200

    Update 2-tv-shows Modal

--- src/2-tv-shows/Modal.js
+++ src/2-tv-shows/Modal.js
@@ -1,13 +1,13 @@
 export class Modal {
   constructor(imgSrc) {
-    // Implement Modal constructor function
+    // TODO
   }
 
   open() {
-    // open function: add Modal instance to DOM
+    // TODO
   }
 
   close() {
-    // close function: remove Modal instance from DOM
+    // TODO
   }
 }
