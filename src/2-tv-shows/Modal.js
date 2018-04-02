c70e8c9de338a64cc89ec7b7718df2289db56e6e
Nicolas Payot <nicolas.payot@gmail.com>
2018-04-02 17:55:51 +0200

    Add corrections for 2-tv-shows

--- src/2-tv-shows/Modal.js
+++ src/2-tv-shows/Modal.js
@@ -1,13 +1,33 @@
 export class Modal {
   constructor(imgSrc) {
-    // TODO
+    this.fragment = document.createDocumentFragment();
+
+    this.modalOverlay = document.createElement('div');
+    this.modalOverlay.className = 'modal-overlay';
+
+    this.modalContent = document.createElement('div');
+    this.modalContent.className = 'modal-content';
+
+    this.modalImg = document.createElement('img');
+    this.modalImg.src = imgSrc;
+    this.modalImg.className = 'modal-img';
+
+    this.closeButton = document.createElement('button');
+    this.closeButton.className = 'close-button';
+    this.closeButton.innerHTML = '<i class="fa fa-times fa-3x"></i>';
+    this.closeButton.onclick = () => this.close();
   }
 
   open() {
-    // TODO
+    this.modalContent.appendChild(this.modalImg);
+    this.modalContent.appendChild(this.closeButton);
+    this.fragment.appendChild(this.modalOverlay);
+    this.fragment.appendChild(this.modalContent);
+    document.body.appendChild(this.fragment);
   }
 
   close() {
-    // TODO
+    document.body.removeChild(this.modalOverlay);
+    document.body.removeChild(this.modalContent);
   }
 }
--- src/2-tv-shows/main.js
+++ src/2-tv-shows/main.js
@@ -1,1 +1,31 @@
-// TODO
+import { Modal } from './Modal.js';
+
+const imgs = Array.from(document.querySelectorAll('.poster'));
+
+imgs.forEach(img => {
+  img.onclick = () => {
+    const modal = new Modal(img.attributes.src.value);
+    modal.open();
+  };
+});
+
+const searchTVShow = e => {
+  const value = e.target.value;
+
+  const keywords = value.toUpperCase();
+  imgs.forEach(img => {
+    img.classList.remove('is-hidden');
+    if (keywords.length <= 2) {
+      return;
+    }
+    const imgKeywords = img.dataset.keywords.toUpperCase();
+    if (!imgKeywords.includes(keywords)) {
+      img.classList.add('is-hidden');
+    } else {
+      img.classList.remove('is-hidden');
+    }
+  });
+};
+
+const searchInput = document.querySelector('[name="search-input"]');
+searchInput.addEventListener('input', searchTVShow);
